187eee61adc9d62f0ac44dbb3f887153f8588bde
Yongwei Yuan <yuan311@purdue.edu>
2024-10-03 13:54:41 -0400

    implement automata quotient operation

--- automata/builder.ml
+++ automata/builder.ml
@@ -33,9 +33,37 @@ module F (L : Literal.T) = struct
 
     let edge_attributes e = [ `Label (L.layout @@ E.label e) ]
     let get_subgraph _ = None
+
+    let inter (v1, g1) (v2, g2) =
+      let rec aux v1 v2 g =
+        let v = v1 @ v2 in
+        if mem_vertex g v then g
+        else
+          fold_succ_e
+            (fun (_, l1, u1) g ->
+              fold_succ_e
+                (fun (_, l2, u2) g ->
+                  match L.(notbot_opt ~substs:[] @@ mk_and l1 l2) with
+                  | Some l ->
+                      add_edge_e (aux u1 u2 g) @@ E.create v l @@ u1 @ u2
+                  | None -> g)
+                g2 v2 g)
+            g1 v1
+          @@ add_vertex g v
+      in
+      aux v1 v2 empty
   end
 
   module Dot = Graph.Graphviz.Dot (G)
+  module PathCheck = Graph.Path.Check (G)
+
+  module EdgeMapper =
+    Graph.Gmap.Edge
+      (G)
+      (struct
+        include G
+        include Graph.Builder.P (G)
+      end)
 
   type t = { init : G.V.t; finals : G.V.t list; graph : G.t }
 
@@ -50,9 +78,10 @@ module F (L : Literal.T) = struct
     fprintf ppf "@[<v 2>init:@ %a@ finals:@ %a@ graph:@ %a@]" pp_regexes init
       (pp_print_list pp_regexes) finals Dot.fprint_graph graph
 
-  let to_dot_file filename {init; finals; graph} =
+  let to_dot_file filename { init; finals; graph } =
     let oc = open_out filename in
-    G.init := init; G.finals := finals;
+    G.init := init;
+    G.finals := finals;
     Dot.output_graph oc graph;
     close_out oc
 
@@ -98,23 +127,7 @@ module F (L : Literal.T) = struct
 
   (** intersect two automaton structures *)
   let intersect a1 a2 =
-    let rec aux v1 v2 g =
-      let v = v1 @ v2 in
-      if G.mem_vertex g v then g
-      else
-        G.fold_succ_e
-          (fun (_, l1, v1') g ->
-            G.fold_succ_e
-              (fun (_, l2, v2') g ->
-                match L.(notbot_opt ~substs:[] @@ mk_and l1 l2) with
-                | Some l ->
-                    G.add_edge_e (aux v1' v2' g) @@ G.E.create v l @@ v1' @ v2'
-                | None -> g)
-              a2.graph v2 g)
-          a1.graph v1
-        @@ G.add_vertex g v
-    in
-    let graph = aux a1.init a2.init G.empty in
+    let graph = G.inter (a1.init, a1.graph) (a2.init, a2.graph) in
     let finals =
       List.fold_product
         (fun finals v1 v2 ->
@@ -123,4 +136,40 @@ module F (L : Literal.T) = struct
         a1.finals a2.finals []
     in
     { init = a1.init @ a2.init; finals; graph }
+
+  (** compute the quotient of [numerator] over [denominator] with
+      [start_num] being the initial states; the result quotient is
+      expressed as a disjunction between {i partial} quotients, which
+      takes the form of a non-deterministic choice, as embedded in
+      [Choice.t], among states in [numerator] that denote the initial
+      state of the associated partial quotients, along with automata
+      that witness the quotienting. *)
+  let quotient (init_num, dist_to_finals, numerator) denominator =
+    let size_num = List.length numerator.init in
+    let init = init_num @ denominator.init in
+    let graph =
+      G.inter (init_num, numerator.graph) (denominator.init, denominator.graph)
+    in
+    let pathchecker = PathCheck.create graph in
+    let finals =
+      G.fold_vertex
+        (fun v acc ->
+          let v_den = List.drop size_num v in
+          if List.mem ~eq:G.V.equal v_den denominator.finals then v :: acc
+          else acc)
+        graph []
+      |> List.sort (fun v u ->
+             Int.compare (dist_to_finals v) (dist_to_finals u))
+    in
+    let edge_filter final (v, l, u) =
+      if
+        PathCheck.check_path pathchecker v final
+        && PathCheck.check_path pathchecker u final
+      then Some (v, l, u)
+      else None
+    in
+    finals |> Choice.of_list
+    |> Choice.map @@ fun final ->
+       let graph = EdgeMapper.filter_map (edge_filter final) graph in
+       ({ init; finals = [ final ]; graph }, final)
 end
