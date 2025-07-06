c9f51b94eecf98940f1222d9789a110bc121b223
Yongwei Yuan <yuan311@purdue.edu>
2025-07-06 00:04:22 -0700

    refactor: update transducer module with enhanced label and state handling

--- stateMachine/transducer.ml
+++ stateMachine/transducer.ml
@@ -7,22 +7,22 @@ open Zzdatatype.Datatype
 open Sugar
 open Sig
 
+module State = struct
+  type t = Final | Normal [@@deriving sexp, compare, equal, hash]
+end
+
 module F (A : ELA) = struct
-  type state = Final | Normal [@@deriving sexp, compare, equal, hash]
-
   module Label = struct
-    type t = A.pred * A.func list [@@deriving sexp, compare]
+    (** [Epsilon] label bears witness to [A.pred] but does not take
+        input *)
+    type t = Pred of A.pred * A.func list | Epsilon of A.P.prop * A.ev list
+    [@@deriving sexp, compare]
 
-    let default = (A.mk_bot, [])
+    let default = Pred (A.mk_bot, [])
   end
 
   module G = struct
-    include
-      Graph.Persistent.Digraph.AbstractLabeled
-        (struct
-          type t = state
-        end)
-        (Label)
+    include Graph.Persistent.Digraph.AbstractLabeled (State) (Label)
 
     let comb v1 v2 =
       V.create
@@ -56,6 +56,7 @@ module F (A : ELA) = struct
         g []
   end
 
+  include State
   module PathCheck = Graph.Path.Check (G)
 
   type sft = { init : G.V.t; g : G.t }
@@ -92,19 +93,35 @@ module F (A : ELA) = struct
     Sys.remove tmp_dot;
     Sys.remove tmp_pdf
 
-  let map f1 f2 { init; g } =
-    { init; g = G.map_labels (fun (l1, l2) -> (f1 l1, List.map f2 l2)) g }
+  let map ~f_pred ~f_func ~f_prop ~f_ev { init; g } =
+    {
+      init;
+      g =
+        G.map_labels
+          (function
+            | Pred (p, fns) -> Pred (f_pred p, List.map f_func fns)
+            | Epsilon (phi, evs) -> Epsilon (f_prop phi, List.map f_ev evs))
+          g;
+    }
 
-  let fold f1 f2 { init; g } acc =
-    G.fold_labels (fun (l1, l2) -> f1 l1 >> List.fold_right f2 l2) g acc
+  let fold ~f_pred ~f_func ~f_prop ~f_ev { init; g } acc =
+    G.fold_labels
+      (function
+        | Pred (p, fns) -> f_pred p >> List.fold_right f_func fns
+        | Epsilon (phi, evs) -> f_prop phi >> List.fold_right f_ev evs)
+      g acc
 
   let clean_moves ~is_bot { init; g } =
     {
       init;
       g =
         G.filter_map_labels
-          (fun (p, fns) ->
-            Option.map (fun p -> (p, fns)) @@ A.simp_opt ~is_bot p)
+          (function
+            | Pred (p, fns) ->
+                Option.map (fun p -> Label.Pred (p, fns))
+                @@ A.simp_opt ~is_bot p
+            | Epsilon (phi, evs) when is_bot phi -> None
+            | Epsilon (phi, evs) -> Some (Label.Epsilon (phi, evs)))
           g;
     }
 
@@ -122,12 +139,12 @@ module F (A : ELA) = struct
 
   let mk_atom p fns =
     let init = G.V.create Normal in
-    let trans = G.E.create init (p, fns) (G.V.create Final) in
+    let trans = G.E.create init (Pred (p, fns)) (G.V.create Final) in
     clean_states { init; g = G.add_edge_e G.empty trans }
 
   let mk_star_atom p fns =
     let init = G.V.create Final in
-    let loop = G.E.create init (p, fns) init in
+    let loop = G.E.create init (Pred (p, fns)) init in
     clean_states { init; g = G.add_edge_e G.empty loop }
 
   (** how to check graph equality *)
@@ -136,7 +153,12 @@ module F (A : ELA) = struct
   let mk_any = mk_star_atom A.mk_top []
 
   let mk_dom { init; g } =
-    let g = G.map_labels (fun (p, fns) -> (p, [])) g in
+    let g =
+      G.map_labels
+        (function
+          | Pred (p, _) -> Pred (p, []) | Epsilon (phi, _) -> Epsilon (phi, []))
+        g
+    in
     { init; g }
 
   (** assume the image of functions labeled on a transition to be
@@ -146,16 +168,21 @@ module F (A : ELA) = struct
       G.fold_edges_e
         (fun e g ->
           let u, v = (G.E.src e, G.E.dst e) in
-          let p, fns = G.E.label e in
-          let ps = List.map (A.mk_image p) fns in
-          snd
-          @@ List.fold_lefti
-               (fun (u, g) i p ->
-                 let v =
-                   if i + 1 = List.length ps then v else G.V.create Normal
-                 in
-                 (v, G.add_edge_e g @@ G.E.create u (p, []) v))
-               (u, g) ps)
+          let with_inputs ps =
+            snd
+            @@ List.fold_lefti
+                 (fun (u, g) i p ->
+                   let v =
+                     if i + 1 = List.length ps then v else G.V.create Normal
+                   in
+                   (v, G.add_edge_e g @@ G.E.create u (Pred (p, [])) v))
+                 (u, g) ps
+          in
+          match G.E.label e with
+          | Pred (p, fns) -> with_inputs @@ List.map (A.mk_image p) fns
+          | Epsilon (phi, evs) ->
+              with_inputs
+              @@ List.map (A.mk_and (A.from_prop phi) << A.ev_to_pred) evs)
         g G.empty
     in
     clean_states { init; g }
@@ -201,7 +228,7 @@ module F (A : ELA) = struct
       unsatisfiable guards and finally eliminates all deadends
       (deadlock states: states from which no final state is
       reachable) *)
-  let mk_compose ~simp_pred { init = init1; g = g1 } { init = init2; g = g2 } =
+  let mk_compose ~is_bot { init = init1; g = g1 } { init = init2; g = g2 } =
     let init = G.comb init1 init2 in
     let g = G.add_vertex G.empty init in
     let rec dfs ~m ~g v1 v2 v =
@@ -211,26 +238,63 @@ module F (A : ELA) = struct
           let m = M.add (v1, v2) v m in
           G.fold_succ_e
             (fun e1 g ->
-              let p1, fns1 = G.E.label e1 in
-              let v1' = G.E.dst e1 in
-              dfs ~m ~g (`Wait (p1, fns1, v1', [], [])) v2 v)
+              match G.E.label e1 with
+              | Pred (p1, fns1) ->
+                  let v1' = G.E.dst e1 in
+                  dfs ~m ~g (`Wait (p1, fns1, v1', A.mk_top, [])) v2 v
+              | Epsilon (phi1, evs1) ->
+                  let v1' = G.E.dst e1 in
+                  dfs ~m ~g (`WaitEpsilon (evs1, v1', A.P.mk_true, [])) v2 v)
             g1 v1 g
-      | `Wait (p1, [], v1, preds, fns) -> (
+      | `Wait (p1, [], v1, pred, fns) -> (
           let v' = G.comb v1 v2 in
-          let pred = A.mk_and_list (p1 :: preds) in
-          match simp_pred pred with
+          let pred = A.mk_and pred p1 in
+          match A.simp_opt ~is_bot pred with
           | Some pred ->
-              let g = G.add_edge_e g @@ G.E.create v (pred, fns) v' in
+              let g = G.add_edge_e g @@ G.E.create v (Pred (pred, fns)) v' in
               dfs ~m ~g (`Go v1) v2 v'
           | None -> g)
-      | `Wait (p1, fn1 :: fns1, v1, preds, fns) ->
+      | `Wait (p1, fn1 :: fns1, v1, pred, fns) ->
           G.fold_succ_e
             (fun e2 g ->
-              let p2, fns2 = G.E.label e2 in
-              let v2' = G.E.dst e2 in
-              let preds = A.mk_preimage p2 fn1 :: preds in
-              let fns = fns @ List.map (fun fn2 -> A.compose fn2 fn1) fns2 in
-              dfs ~m ~g (`Wait (p1, fns1, v1, preds, fns)) v2 v)
+              match G.E.label e2 with
+              | Pred (p2, fns2) ->
+                  let v2' = G.E.dst e2 in
+                  let pred = A.(mk_and pred @@ mk_preimage p2 fn1) in
+                  let fns =
+                    fns @ List.map (fun fn2 -> A.compose fn2 fn1) fns2
+                  in
+                  dfs ~m ~g (`Wait (p1, fns1, v1, pred, fns)) v2' v
+              | Epsilon (phi2, evs2) ->
+                  let v2' = G.E.dst e2 in
+                  let pred = A.(mk_and pred @@ from_prop phi2) in
+                  let fns = fns @ List.map A.mk_const evs2 in
+                  dfs ~m ~g (`Wait (p1, fn1 :: fns1, v1, pred, fns)) v2' v)
+            g2 v2 g
+      | `WaitEpsilon ([], v1, phi, evs) ->
+          let v' = G.comb v1 v2 in
+          let g = G.add_edge_e g @@ G.E.create v (Epsilon (phi, evs)) v' in
+          dfs ~m ~g (`Go v1) v2 v'
+      | `WaitEpsilon (ev1 :: evs1, v1, phi, evs) ->
+          G.fold_succ_e
+            (fun e2 g ->
+              match G.E.label e2 with
+              | Pred (p2, fns2) ->
+                  let v2' = G.E.dst e2 in
+                  let phi =
+                    A.(
+                      P.mk_and phi @@ to_prop @@ mk_preimage p2 @@ mk_const ev1)
+                  in
+                  let compose_with_ev1 fn2 =
+                    A.(force_const @@ compose fn2 @@ mk_const ev1)
+                  in
+                  let evs = evs @ List.map compose_with_ev1 fns2 in
+                  dfs ~m ~g (`WaitEpsilon (evs1, v1, phi, evs)) v2' v
+              | Epsilon (phi2, evs2) ->
+                  let v2' = G.E.dst e2 in
+                  let phi = A.P.mk_and phi phi2 in
+                  let evs = evs @ evs2 in
+                  dfs ~m ~g (`WaitEpsilon (ev1 :: evs1, v1, phi, evs)) v2' v)
             g2 v2 g
     in
     let g = dfs ~m:M.empty ~g (`Go init1) init2 init in
@@ -246,24 +310,40 @@ module F (A : ELA) = struct
     let rec dfs ~m ~g v1 v2 v =
       if M.mem (v1, v2) m then g
       else
+        let m = M.add (v1, v2) v m in
         G.fold_succ_e
-          (fun e1 ->
+          (fun e1 g ->
             let v1' = G.E.dst e1 in
-            let p1, fns = G.E.label e1 in
-            G.fold_succ_e
-              (fun e2 g ->
-                let v2' = G.E.dst e2 in
-                let p2, nil = G.E.label e2 in
-                assert (List.is_empty nil);
-                let v' = G.comb v1' v2' in
-                let p = A.mk_and p1 p2 in
-                match simp_pred p with
-                | Some p ->
-                    dfs ~m
-                      ~g:(G.add_edge_e g @@ G.E.create v (p, fns) v')
-                      v1' v2' v'
-                | None -> g)
-              sfa v2)
+            match G.E.label e1 with
+            | Pred (p1, fns1) ->
+                G.fold_succ_e
+                  (fun e2 g ->
+                    let v2' = G.E.dst e2 in
+                    match[@warning "-8"] G.E.label e2 with
+                    | Epsilon (phi, evs) ->
+                        let v' = G.comb v1 v2' in
+                        let g =
+                          G.add_edge_e g @@ G.E.create v (Epsilon (phi, evs)) v'
+                        in
+                        dfs ~m ~g v1 v2' v'
+                    | Pred (p2, []) -> (
+                        let v' = G.comb v1' v2' in
+                        let p = A.mk_and p1 p2 in
+                        match simp_pred p with
+                        | None -> g
+                        | Some p ->
+                            dfs ~m
+                              ~g:
+                                (G.add_edge_e g
+                                @@ G.E.create v (Pred (p, fns1)) v')
+                              v1' v2' v'))
+                  sfa v2 g
+            | Epsilon (phi, evs) ->
+                let v' = G.comb v1' v2 in
+                let g =
+                  G.add_edge_e g @@ G.E.create v (Epsilon (phi, evs)) v'
+                in
+                dfs ~m ~g v1' v2 v')
           sft v1 g
     in
     let g = dfs ~m:M.empty ~g init1 init2 init in
